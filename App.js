cd6714e5ecc8672f5ee8f0b8d00194f709fa8514
Alexander Schelchere <ascellie@gmail.com>
2018-06-01 14:26:04 -0400

    Able to navigate through app - Nothing much to see yet!

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { createStackNavigator, StackNavigator } from "react-navigation";
 
 export default class App extends React.Component {
   render() {
+    return <RootStack />;
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the next page!</Text>
+      </View>
+    );
+  }
+}
+
+class Home extends React.Component {
+  render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.text}>
+          Okay, so this is the first page of the app!
+        </Text>
+        <Button
+          title="Learn More"
+          onPress={() => this.props.navigation.navigate("Main")}
+        />
       </View>
     );
   }
@@ -16,8 +37,29 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#00D1FF",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  text: {
+    fontSize: 24
+  },
+  button: {
+    backgroundColor: "#FF7373",
+    fontSize: 12
+  }
 });
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    Main: {
+      screen: Main
+    }
+  },
+  {
+    initialRoutName: "Home"
+  }
+);
